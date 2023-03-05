5bb783f35ad94401f43c7b9ca0099eb3f8cf91c0
Soufiane Ben-zarga <sben-zar@e2r2p9.1337.ma>
2023-03-05 01:05:03 +0100

    fix vagrantfile

--- p1/scripts_config/server_setup.sh
+++ p1/scripts_config/server_setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sudo su
+vagrant plugin install vagrant-scp
 echo "echo instaling k3s on The Server mode..."
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - --flannel-iface=eth1
 echo "waiting for the token to be generated..."
 sleep 5
 sudo cat /var/lib/rancher/k3s/server/token > /home/vagrant/token
+
+echo "****************************************"
+echo "****************************************"
+echo "****************************************"
+echo "****************************************"
+
+# vagrant scp sbenzarS:/home/vagrant/token .
--- p1/scripts_config/sworker_setup.sh
+++ p1/scripts_config/sworker_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo su
-export  K3S_TOKEN_FILE=/vagrant/token
-export  K3S_URL=https://192.168.56.110:6443
-export  INSTALL_K3S_EXEC="--flannel-iface eth1"
-curl -sfL https://get.k3s.io  sh -
+# export  K3S_TOKEN_FILE=/vagrant/token
+# export  K3S_URL=https://192.168.56.110:6443
+# export  INSTALL_K3S_EXEC="--flannel-iface eth1"
+# curl -sfL https://get.k3s.io  sh -
+
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN_FILE=/vagrant/token INSTALL_K3S_EXEC="--flannel-iface eth1" sh -
